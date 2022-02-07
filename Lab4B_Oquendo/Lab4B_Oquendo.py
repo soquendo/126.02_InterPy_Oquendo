e67f946ef67ec55a0c74b28e721f4c38b75b76aa
Oquendo <soquendo@email.neit.edu>
2022-02-07 09:39:15 -0500

    lab 4b edits

--- Lab4B_Oquendo/Lab4B_Oquendo.py
+++ Lab4B_Oquendo/Lab4B_Oquendo.py
@@ -11,12 +11,12 @@ def seatMap(aA, bB, cC, dD):
 		print("	Row", r, " | ", aA[r]," ", bB[r], "|   |", cC[r], " ", dD[r], "\n")
 
 def selectRow():
-	row = int(input("What row would you like to choose? [1-7]: "))
+	row = int(input(" What row would you like to choose? [1-7]: "))
 
 	return row
 
 def selectSeat():
-	seat = input("What seat would you like? [ex. A-D]: ").upper()
+	seat = input(" What seat would you like? [ex. A-D]: ").upper()
 
 	return seat
 
@@ -24,18 +24,28 @@ def seatAssign(row, seat, aA, bB, cC, dD):
 
 	if seat == "A" and aA[row] != "X":
 		aA[row] = "X"
+		print(" \nYour reservation has been placed.")
 	if seat == "B" and bB[row] != "X":
 		bB[row] = "X"
+		print(" \nYour reservation has been placed.")
+	if seat == "C" and cC[row] != "X":
+		cC[row] = "X"
+		print(" \nYour reservation has been placed.")
+	if seat == "D" and dD[row] != "X":
+		dD[row] = "X"
+		print(" \nYour reservation has been placed.")
+	if seat == "X":
+		print(" \n *ERROR* This seat is already reserved, please select another: ")
 
-	else:
-		print("This seat is already reserved, please select another: ")
+	#else:
+		#print(" \n *ERROR* This seat is already reserved, please select another: ")
+		
 
 
 
 #----------- MAIN -------------
 
-print("	   Welcome to NEIT Airways\n")
-print("		 SEATING CHART\n\n")
+
 
 aisleA = ["", "A", "A", "A", "A", "A", "A", "A"]
 aisleB = ["", "B", "B", "B", "B", "B", "B", "B"]
@@ -45,11 +55,16 @@ aisleD = ["", "D", "D", "D", "D", "D", "D", "D"]
 answer = "y"
 while answer == "y":
 
+	print("		 SEATING CHART\n\n")
+
 	seatMap(aisleA, aisleB, aisleC, aisleD)
+	print("\n")
 
 	row = selectRow()
 	seat = selectSeat()
 
 	seatAssign(row, seat, aisleA, aisleB, aisleC, aisleD)
+	print("\n")
+	
 
 	
